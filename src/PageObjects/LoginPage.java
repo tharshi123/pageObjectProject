3d6702d2eed79c9bc811f05d3bfe39239120ab1e
tharshi <jeytharshi@gmail.com>
2018-06-19 10:07:26 -0500

    tharshipageobject

---
+++ src/PageObjects/LoginPage.java
@@ -0,0 +1,40 @@
+package PageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+  
+	WebDriver driver;
+	By userNameEle = By.id("email");
+	By passwordEle = By.id("pass");
+	By loginButton =By.id("loginbutton");
+	
+	
+	public LoginPage(WebDriver driver) {
+		this.driver= driver;
+		
+	}
+	
+	public void setUsername(String username) {
+		driver.findElement(userNameEle).sendKeys(username);
+	}
+	
+	public void setPassword (String password) {
+		driver.findElement(passwordEle).sendKeys(password);
+	}
+	
+	public void login(String username, String password)
+	{
+		
+		this.setUsername(username);
+		this.setPassword(password);
+		driver.findElement(loginButton).click();
+	}
+	
+	public String getpageTitle() {
+		return driver.getTitle();
+		
+	}
+	
+}
---
+++ src/tharshiPOM/TestLoginTwo.java
@@ -0,0 +1,42 @@
+package tharshiPOM;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import PageObjects.LoginPage;
+
+public class TestLoginTwo {
+	
+	public WebDriver driver;
+	public String driverPath = "C:\\Users\\Tharshini\\Downloads\\chromedriver_win32\\chromedriver.exe";
+	
+	LoginPage lfb;
+	
+
+	@BeforeTest
+	public void setUp() {
+		
+	
+	System.setProperty("webdriver.chrome.driver",driverPath );
+	driver = new ChromeDriver();
+
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    driver.get("http://www.facebook.com");
+
+	}
+	
+	@Parameters({ "username", "password" })
+	@Test
+	public void loginIntoFB(String username , String password)
+	{
+		lfb = new LoginPage(driver);
+		lfb.login(username, password);
+		System.out.println("PageTitle: " + lfb.getpageTitle());
+	}
+	
+}
